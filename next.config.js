8712eca1e3daf819fc0de55ad7acb852b0463424
Sergei Shch <sektarford@gmail.com>
2023-09-15 18:23:16 +0200

    Change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
   reactStrictMode: true,
   images: {
     domains: ["drive.google.com"],
